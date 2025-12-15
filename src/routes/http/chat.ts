144dab5486a40fc08bec32b52af030a563922c8c
A-Eugene <amadeuseugenedirgantara@gmail.com>
2025-12-16 00:54:52 +0700

    Test push (+ POST route for sendMessage)

--- src/routes/http/chat.ts
+++ src/routes/http/chat.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { chatAuthorizationMiddleware } from "../../middlewares/chat-authorization-middleware";
+import { fileUploadMiddleware } from "../../middlewares/file-middleware";
+import { ChatController } from "../../controllers/chat-controller";
 
 export const chatRoutes = express.Router();
 
 // For /chats/:chatId routes
 chatRoutes.use(chatAuthorizationMiddleware as unknown as express.RequestHandler);
-// privateRouter.post("/chat/:chatId/messages", fileUploadMiddleware.array("images"), ChatController.sendMessage);
+chatRoutes.post("/chat/:chatId/messages", fileUploadMiddleware.array("images"), ChatController.sendMessage);
